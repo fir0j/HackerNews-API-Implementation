b6a6143fbee744df3da1308c92903104d071ea0a
fir0j <firoj.is.available@gmail.com>
2020-05-03 16:46:59 +0530

    Updates

---
+++ precache-manifest.998767f7534ce696228b6576bd3432df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cabf0b0611c7560f4351128ce9168d4",
+    "url": "/HackerNews-API-Implementation/index.html"
+  },
+  {
+    "revision": "f75283b70116d41c5faf",
+    "url": "/HackerNews-API-Implementation/static/js/2.e1bbcd68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HackerNews-API-Implementation/static/js/2.e1bbcd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040eef66e6956c614fd3",
+    "url": "/HackerNews-API-Implementation/static/js/main.b131e2e7.chunk.js"
+  },
+  {
+    "revision": "a7e233154f0638b6a4a0",
+    "url": "/HackerNews-API-Implementation/static/js/runtime-main.f8f43907.js"
+  }
+]);
